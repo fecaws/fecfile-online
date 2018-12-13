6ef0f070294febd22d39bc06f60ddec97a22158e
Roberto <Roberto.Sanchez@ctr.salientcrgt.com>
2018-12-13 14:39:52 -0500

    wait for db to be able to accept connections

--- data/flyway_migration.sh
+++ data/flyway_migration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-flyway -url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -user=$DB_NAME -password=$DB_PASSWORD -schemas=public migrate
+while [ flyway -url=jdbc:postgresql://$DB_HOST:$DB_PORT/$DB_NAME -user=$DB_NAME -password=$DB_PASSWORD -schemas=public migrate -ne 0]; do fc -s ; done
