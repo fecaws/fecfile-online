70861906baa985d44e11c21d8144c08cdb82f247
mbasupally <naga.basupally@salientcrgt.com>
2019-01-07 14:09:07 -0500

    Added more ref tables, reports and form_3x table

---
+++ data/migrations/V00004.1__ref_rpt_fix.sql
@@ -0,0 +1,29 @@
+﻿ALTER TABLE public.ref_rpt_types DROP COLUMN last_update_date;
+
+ALTER TABLE public.ref_rpt_types
+    ADD COLUMN regular_special_report_ind character varying(1);
+
+ALTER TABLE public.ref_rpt_types
+    ADD COLUMN last_update_date timestamp DEFAULT NOW(); 
+    
+update ref_rpt_types
+	set regular_special_report_ind='R',
+    	last_update_date=current_time
+	where rpt_type like 'M%' or rpt_type  like 'Q%'  or rpt_type ='24' or rpt_type='48' or  rpt_type='TER';
+
+update ref_rpt_types
+	set regular_special_report_ind='S',
+        last_update_date=current_time
+  where regular_special_report_ind is null;
+
+
+select * from due_dates where cvg_end_date is NOT null 
+
+
+ALTER TABLE public.due_dates DROP COLUMN last_update_date;
+
+ALTER TABLE public.due_dates
+    ADD COLUMN report_year integer;
+
+ALTER TABLE public.due_dates
+    ADD COLUMN last_update_date timestamp DEFAULT NOW(); 
