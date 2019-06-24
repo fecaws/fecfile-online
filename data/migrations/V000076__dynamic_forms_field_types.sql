f8d8843e8af70aa99bab594847061c362e0b9ba8
Craig Stroman <cstroman@gmail.com>
2019-06-24 17:05:11 -0400

    Adding migration

---
+++ data/migrations/V000076__dynamic_forms_field_types.sql
@@ -0,0 +1,3 @@
+
+update public.dynamic_form_fields set field_type = 'text' where field_db_name='contribution_aggregate';
+update public.dynamic_form_fields set field_type = 'text' where field_db_name='contribution_amount';
