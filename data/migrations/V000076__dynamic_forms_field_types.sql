4a5f9edd79af8260909e3df12873df484bd52c60
Craig Stroman <cstroman@gmail.com>
2019-06-25 10:30:21 -0400

    Combining and removing migrations

--- data/migrations/V000076__dynamic_forms_field_types.sql
+++
@@ -1,2 +0,0 @@
-update public.dynamic_form_fields set field_type = 'text' where field_db_name='contribution_aggregate';
-update public.dynamic_form_fields set field_type = 'text' where field_db_name='contribution_amount';
