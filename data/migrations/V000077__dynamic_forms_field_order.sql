99e2637b57095787b926ea607eea519116e800ea
Craig Stroman <cstroman@gmail.com>
2019-06-26 14:23:49 -0400

    Removing parseInt and adding parseFloat in contributionAmountChange function

--- data/migrations/V000077__dynamic_forms_field_order.sql
+++ data/migrations/V000077__dynamic_forms_field_order.sql
@@ -1,8 +1,8 @@
 
 update public.dynamic_form_fields set field_section = 'Name Section 1', field_section_order = 1 where field_db_name='middle_name';
 
-update public.dynamic_form_fields set field_order = 1 where field_db_name = 'last_name';
-update public.dynamic_form_fields set field_order = 2 where field_db_name = 'first_name';	
+update public.dynamic_form_fields set field_order = 1, seperator = 'true' where field_db_name = 'last_name';
+update public.dynamic_form_fields set field_order = 2, seperator = 'true' where field_db_name = 'first_name';	
 update public.dynamic_form_fields set field_order = 3 where field_db_name = 'middle_name';	
 update public.dynamic_form_fields set field_order = 4 where field_db_name = 'preffix';	
 update public.dynamic_form_fields set field_order = 5 where field_db_name = 'suffix';	
