09c4d02385619524c611bff8528fff1506a5f3f1
develop <praveen.jinka@ctr.salientcrgt.com>
2019-11-13 17:07:53 -0500

    minor

--- data/migrations/V0000205__dynamic_form_field_updates.sql
+++ data/migrations/V0000205__dynamic_form_field_updates.sql
@@ -1,3 +1,4 @@
+
 UPDATE public.dynamic_form_fields SET toggle = true, entity_group = 'org-group' WHERE transaction_type = 'ALLOC_FEA_DISB_DEBT' and field_db_name='entity_name';
 
 UPDATE public.dynamic_form_fields SET field_value = '/sh6/schedH6' WHERE field_db_name='api_call' AND transaction_type='ALLOC_FEA_DISB_DEBT';
