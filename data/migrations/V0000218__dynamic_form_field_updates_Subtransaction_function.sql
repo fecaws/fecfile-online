e70b0760a99340187a5dcfa61210cc77d37ad73b
develop <praveen.jinka@ctr.salientcrgt.com>
2019-12-03 10:50:56 -0500

    adding in entity_group to SH6

--- data/migrations/V0000218__dynamic_form_field_updates_Subtransaction_function.sql
+++ data/migrations/V0000218__dynamic_form_field_updates_Subtransaction_function.sql
@@ -132,3 +132,5 @@ CREATE OR REPLACE VIEW public.dynamic_forms_view AS
    FROM dy_forms_by_section dfbs
   GROUP BY dfbs.form_type, dfbs.transaction_type;
 
+UPDATE public.dynamic_form_fields SET entity_group = 'ind-group' WHERE field_db_name in ('last_name', 'first_name', 'middle_name', 'prefix', 'suffix') and transaction_type in 
+('ALLOC_FEA_DISB_DEBT','ALLOC_FEA_DISB','ALLOC_FEA_STAF_REIM','ALLOC_FEA_VOID','ALLOC_FEA_CC_PAY_MEMO','ALLOC_FEA_STAF_REIM_MEMO');
