955cc3a85455c9dc976fd2fd0caf689dd718a269
jpraveenkmrSalient <41488234+jpraveenkmrSalient@users.noreply.github.com>
2019-12-11 09:11:52 -0500

    Fne transactions fix for amend (#1639)
    * sched F and get_all_transactions API issues fixed
    
    * get all transactions API
    
    * Modifying get_all_transactions API and trash_restore_transactions API
    
    * handling default value in get_all_transactions API
    
    * Modifying CRUD APIs to address null values in deleteddate column
    
    * bug fix
    
    * sql modifications for receipts, disbursements, loans, others views
    
    * CRUD APIs Bug Fix
    
    * modified get_all_transactions and delete_trashed_transactions API
    
    * modifying all receipts and disbursements views

---
+++ data/migrations/V0000223__receipts_disbursements_updates.sql
@@ -0,0 +1,109 @@
+DROP VIEW public.all_receipts_transactions_view;
+
+CREATE OR REPLACE VIEW public.all_receipts_transactions_view AS 
+ SELECT sa.cmte_id,
+    sa.report_id,
+    rp.report_type,
+    rrt.rpt_type_desc AS report_desc,
+    sa.line_number,
+    'sched_a'::text AS transaction_table,
+    sa.transaction_type,
+    rt.tran_desc AS transaction_type_desc,
+    sa.transaction_type_identifier,
+    sa.transaction_id,
+    sa.back_ref_transaction_id,
+    sa.back_ref_sched_name,
+    sa.entity_id,
+        CASE
+            WHEN e.entity_type::text = ANY (ARRAY['IND'::character varying::text, 'CAN'::character varying::text]) THEN ((((initcap(e.last_name::text) || COALESCE(', '::text || initcap(e.first_name::text), ''::text)) || COALESCE(', '::text || initcap(e.middle_name::text), ''::text)) || COALESCE(', '::text || initcap(e.preffix::text), ''::text)) || COALESCE(', '::text || initcap(e.suffix::text), ''::text))::character varying
+            ELSE e.entity_name
+        END AS name,
+    e.street_1,
+    e.street_2,
+    e.city,
+    e.state,
+    e.zip_code,
+    e.occupation,
+    e.employer,
+    e.entity_type,
+    sa.contribution_date AS transaction_date,
+    sa.contribution_amount AS transaction_amount,
+    COALESCE(sa.aggregate_amt, '0.0'),
+    sa.purpose_description,
+    sa.memo_code,
+    sa.memo_text,
+    sa.election_code,
+    sa.election_other_description,
+    sa.delete_ind,
+    sa.create_date,
+    sa.last_update_date,
+    sa.itemized_ind AS itemized,
+    '/sa/schedA'::text AS api_call,
+    rt.category_type,
+        CASE
+            WHEN e.delete_ind = 'Y'::bpchar THEN e.last_update_date
+            WHEN sa.delete_ind = 'Y'::bpchar THEN sa.last_update_date
+            ELSE NULL::timestamp without time zone
+        END AS deleteddate
+   FROM sched_a sa
+     JOIN entity e ON e.entity_id::text = sa.entity_id::text
+     JOIN ref_transaction_types rt ON rt.tran_identifier::text = sa.transaction_type_identifier::text
+     JOIN reports rp ON rp.cmte_id::text = sa.cmte_id::text AND rp.report_id = sa.report_id
+     JOIN ref_rpt_types rrt ON rp.report_type::text = rrt.rpt_type::text
+  WHERE sa.transaction_type_identifier::text <> ALL (ARRAY['LEVIN_PARTN_MEMO'::character varying::text, 'LEVIN_TRIB_REC'::character varying::text, 'LEVIN_PARTN_REC'::character varying::text, 'LEVIN_ORG_REC'::character varying::text, 'LEVIN_INDV_REC'::character varying::text, 'LEVIN_NON_FED_REC'::character varying::text, 'LEVIN_OTH_REC'::character varying::text, 'LEVIN_PAC_REC'::character varying::text]);
+
+
+DROP VIEW public.all_disbursements_transactions_view;
+
+CREATE OR REPLACE VIEW public.all_disbursements_transactions_view AS 
+ SELECT sb.cmte_id,
+    sb.report_id,
+    rp.report_type,
+    rrt.rpt_type_desc AS report_desc,
+    sb.line_number,
+    'sched_b'::text AS transaction_table,
+    sb.transaction_type,
+    rt.tran_desc AS transaction_type_desc,
+    sb.transaction_type_identifier,
+    sb.transaction_id,
+    sb.back_ref_transaction_id,
+    sb.back_ref_sched_name,
+    sb.entity_id,
+        CASE
+            WHEN e.entity_type::text = ANY (ARRAY['IND'::character varying::text, 'CAN'::character varying::text]) THEN ((((initcap(e.last_name::text) || COALESCE(', '::text || initcap(e.first_name::text), ''::text)) || COALESCE(', '::text || initcap(e.middle_name::text), ''::text)) || COALESCE(', '::text || initcap(e.preffix::text), ''::text)) || COALESCE(', '::text || initcap(e.suffix::text), ''::text))::character varying
+            ELSE e.entity_name
+        END AS name,
+    e.street_1,
+    e.street_2,
+    e.city,
+    e.state,
+    e.zip_code,
+    e.occupation,
+    e.employer,
+    e.entity_type,
+    sb.expenditure_date AS transaction_date,
+    sb.expenditure_amount AS transaction_amount,
+    COALESCE(sb.aggregate_amt, '0.0'),
+    sb.expenditure_purpose AS purpose_description,
+    sb.memo_code,
+    sb.memo_text,
+    sb.election_code,
+    sb.election_other_description,
+    sb.delete_ind,
+    sb.create_date,
+    sb.last_update_date,
+    sb.itemized_ind AS itemized,
+    '/sb/schedB'::text AS api_call,
+    rt.category_type,
+    sb.election_year,
+    sb.beneficiary_cmte_id,
+        CASE
+            WHEN e.delete_ind = 'Y'::bpchar THEN sb.last_update_date
+            ELSE NULL::timestamp without time zone
+        END AS deleteddate
+   FROM sched_b sb
+     JOIN entity e ON e.entity_id::text = sb.entity_id::text
+     JOIN ref_transaction_types rt ON rt.tran_identifier::text = sb.transaction_type_identifier::text
+     JOIN reports rp ON rp.cmte_id::text = sb.cmte_id::text AND rp.report_id = sb.report_id
+     JOIN ref_rpt_types rrt ON rp.report_type::text = rrt.rpt_type::text
+  WHERE sb.transaction_type_identifier::text <> ALL (ARRAY['LEVIN_VOTER_REG'::character varying::text, 'LEVIN_VOTER_ID'::character varying::text, 'LEVIN_GOTV'::character varying::text, 'LEVIN_GEN'::character varying::text, 'LEVIN_OTH_DISB'::character varying::text]);
