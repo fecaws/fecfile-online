c865bcd2ed1c68a844958332c492cb41e84c05ed
Praveen <praveen.jinka@ctr.salientcrgt.com>
2020-01-31 10:50:26 -0500

    minor fix

--- data/migrations/V0000260__transaction_tree_updates.sql
+++ data/migrations/V0000260__transaction_tree_updates.sql
@@ -1,2 +1,2 @@
 UPDATE public.transaction_categories SET sub_category_type='Contributions/Expenditures to Regular Filers' 
-  WHERE sub_category_type='Contributions/Expenditures to Reg. Filers';
+  WHERE sub_category_type='"Contributions/Expenditures to Regular Filers"';
