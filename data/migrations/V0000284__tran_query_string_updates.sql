e7056d9088db948c366e388259e1040765d5a134
jpraveenkmrSalient <41488234+jpraveenkmrSalient@users.noreply.github.com>
2020-02-20 09:41:27 -0500

    Fne fixes praveen (#2011)
    * adding in edit and delete functionality for reattribution
    
    * adding in migrations'
    g
    
    * resolving back_ref bug
    
    * modifications to reattribution
    
    * modifications to reattribution API
    
    * adding in migrations
    
    * reattribution fix
    
    * JSON builder update to print child transactions
    
    * fixing issues
    
    * updating migrations to fix issues
    
    * migrations fix
    
    * fixing reattribution and json builder issues
    
    * flyway fix

---
+++ data/migrations/V0000284__tran_query_string_updates.sql
@@ -0,0 +1,34 @@
+INSERT INTO public.tran_query_string(form_type, sched_type, tran_type_identifier, query_string)
+VALUES ('F3X', 'SH1', 'ALLOC_H1', '
+                    SELECT COALESCE(t1.line_number, '''') AS "lineNumber",
+                    COALESCE(t1.transaction_type, '''') AS "transactionTypeCode",
+                    COALESCE(t1.transaction_type_identifier, '''') AS "transactionTypeIdentifier",
+                    COALESCE(t1.transaction_id, '''') AS "transactionId",
+                    COALESCE(t1.presidential_only::text, '''') AS "presidentialOnly",
+                    COALESCE(t1.presidential_and_senate::text, '''') AS "presidentialAndSenate",
+                    COALESCE(t1.senate_only::text, '''') AS "senateOnly",
+                    COALESCE(t1.non_pres_and_non_senate::text, '''') AS "nonPresidentialAndNonSenate",
+                    COALESCE(t1.federal_percent, 0.0) AS "federalPercent",
+                    COALESCE(t1.non_federal_percent, 0.0) AS "nonFederalPercent",
+                    COALESCE(t1.administrative::text, '''') AS "administrative",
+                    COALESCE(t1.generic_voter_drive::text, '''') AS "genericVoterDrive",
+                    COALESCE(t1.public_communications::text, '''') AS "publicCommunications"
+                    FROM public.sched_h1 t1
+                    WHERE t1.transaction_type_identifier = ''ALLOC_H1'' AND t1.report_id = %s AND t1.cmte_id = %s AND t1.delete_ind is distinct from ''Y''
+                    ');
+
+INSERT INTO public.tran_query_string(form_type, sched_type, tran_type_identifier, query_string)
+VALUES ('F3X', 'SH2', 'ALLOC_H2_RATIO', '
+                    SELECT COALESCE(t1.line_number, '''') AS "lineNumber",
+                    COALESCE(t1.transaction_type, '''') AS "transactionTypeCode",
+                    COALESCE(t1.transaction_type_identifier, '''') AS "transactionTypeIdentifier",
+                    COALESCE(t1.transaction_id, '''') AS "transactionId",
+                    COALESCE(t1.activity_event_name, '''') AS "activityEventName",
+                    COALESCE(t1.fundraising::text, '''') AS "fundraising",
+                    COALESCE(t1.direct_cand_support::text, '''') AS "directCandidateSupport",
+                    COALESCE(t1.ratio_code, '''') AS "ratioCode",
+                    COALESCE(t1.federal_percent, 0.0) AS "federalPercent",
+                    COALESCE(t1.non_federal_percent, 0.0) AS "nonFederalPercent"
+                    FROM public.sched_h2 t1
+                    WHERE t1.transaction_type_identifier = ''ALLOC_H2_RATIO'' AND t1.report_id = %s AND t1.cmte_id = %s AND t1.delete_ind is distinct from ''Y''
+                    ');
