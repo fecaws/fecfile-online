a6bd136793853bdb4a11e9cf9db3429dd8ecca75
qsunctr <qsun.ctr@fec.gov>
2020-02-24 20:36:40 -0500

    update sched_h4 and sched_h6 memo transactions event field to be readonly

---
+++ data/migrations/V0000292__Update_schedh4h6_memo_event_field_readonly.sql
@@ -0,0 +1,6 @@
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_CC_PAY_MEMO' and field_label = 'Type';
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_CC_PAY_MEMO' and field_label = 'Allocated Activity or Event';
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_STAF_REIM_MEMO' and field_label = 'Type';
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_STAF_REIM_MEMO' and field_label = 'Allocated Activity or Event';
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_PMT_TO_PROL_MEMO' and field_label = 'Type';
+update public.dynamic_form_fields set field_is_readonly = true where transaction_type = 'ALLOC_EXP_PMT_TO_PROL_MEMO' and field_label = 'Allocated Activity or Event';
