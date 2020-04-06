0129ffbd9aaf25d6840b7d120c9a37c88d67c031
Roberto Sanchez <rsanchez@rhythmictech.com>
2020-04-06 15:50:48 -0400

    Added FNE-1938

--- data/migrations/V0000331__Relocating_Tribal_National_Party_Recount.sql
+++ data/migrations/V0000331__Relocating_Tribal_National_Party_Recount.sql
@@ -6,6 +6,8 @@ UPDATE public.transaction_categories SET tran_type_order='10' WHERE tran_type_id
 UPDATE public.transaction_categories SET tran_type_order='11' WHERE tran_type_identifier = 'TRIB_NP_RECNT_ACC';
 
 -- FNE-1656 Fixing purpose description for Unregistered Receipt from Person
+UPDATE dynamic_form_fields set field_is_required = 'True', field_is_readonly = 'True', field_value = 'Permissible Funds'  WHERE transaction_type = 'PAC_NON_FED_REC' AND field_label = 'Receipt Description';
 
+-- FNE-1983 Adding space between Recount:Refund
 
-UPDATE dynamic_form_fields set field_is_required = 'True', field_is_readonly = 'True', field_value = 'Permissible Funds'  WHERE transaction_type = 'PAC_NON_FED_REC' AND field_label = 'Receipt Description'
+UPDATE dynamic_form_fields SET field_value = 'Recount: Refund' WHERE field_value = 'Recount:Refund';
