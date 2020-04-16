2219568d83839715962dd0c5d1679fe9ae613118
Zobair <zsaleem@halvik.com>
2020-04-16 14:01:21 -0400

    fixed issue

--- data/migrations/V0000339__sched_E_dynamic_forms_view_update.sql
+++ data/migrations/V0000339__sched_E_dynamic_forms_view_update.sql
@@ -1,3 +1,3 @@
-update dynamic_form_fields set field_is_required = false where transaction_type like 'IE%' and field_db_name = 'payee_cmte_id'
+update dynamic_form_fields set field_is_required = false where transaction_type like 'IE%' and field_db_name = 'payee_cmte_id';
 
 commit;
