dcf4a3241f943fa173bcbffabd1a2262be5b17d4
Roberto Sanchez <rsanchez@rhythmictech.com>
2020-04-21 13:57:13 -0400

    resolving conflicts

--- front-end/src/environments/environment.awsuat.ts
+++ front-end/src/environments/environment.awsuat.ts
@@ -5,7 +5,7 @@ export const environment = {
   validateSuccess: 'All required fields have passed validation',
   appTitle: 'FECfile',
   awsRegion: 'us-east-1',
-  awsIdentityPoolId: 'us-east-1:f0f414b2-8e9f-4488-9cc1-34a5918a1a1d',
+  awsIdentityPoolId: '######################################',
   ACCESS_KEY: _process.env.ACCESS_KEY,
   SECRET_KEY: _process.env.SECRET_KEY
 };
--- front-end/src/environments/environment.local.ts
+++ front-end/src/environments/environment.local.ts
@@ -5,7 +5,7 @@ export const environment = {
   appTitle: 'FECfile',
   validateSuccess: 'All required fields have passed validation.',
   awsRegion: 'us-east-1',
-  awsIdentityPoolId: 'us-east-1:f0f414b2-8e9f-4488-9cc1-34a5918a1a1d',
+  awsIdentityPoolId: '##################################',
   ACCESS_KEY: _process.env.ACCESS_KEY,
   SECRET_KEY: _process.env.SECRET_KEY
 };
